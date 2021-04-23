500f922b8e760d42d4f85f84a98bc55d2f588b18
Ralph <ralphoburu@gmail.com>
2021-04-23 15:28:01 +0300

    Fix logic

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const form = document.getElementById('form');
 
 const myLibrary = [];
 
+function Book(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+}
+
 function displayForm() {
   if (form.style.display === '') {
     form.style.display = 'block';
@@ -11,36 +17,34 @@ function displayForm() {
   }
 }
 
-function displaybooks(book) {
+function addBookToLibrary(e) {
+  e.preventDefault();
   const li = document.createElement('li');
-  li.className = 'collection-item';
 
-  li.innerHTML = `
-  <h5>Author: <span id="author">${book[0]}</span></h5>
-  <p>Title: ${book[1]} </p>
-  <p>Pages: ${book[2]}</span></p>
+  li.className = 'collection-item';
+  const [title, author, pages] = e.target.elements;
+  const book = new Book(title.value, author.value, pages.value);
+
+  myLibrary.push(book);
+
+  myLibrary.forEach(e => {
+      li.innerHTML = `
+  <h5>Author: <span id="author">${e.author}</span></h5>
+  <p>Title: ${e.title} </p>
+  <p>Pages: ${e.pages}</span></p>
   <a class="delete"><i class="far fa-trash-alt"></i></a>
   <label>
   <input type="checkbox" />
   <span>Read</span>
-</label>
+  </label>
   `;
   books.appendChild(li);
+    
+  });
+
   document.querySelector('#title').value = '';
   document.querySelector('#author').value = '';
   document.querySelector('#pages').value = '';
-}
-
-function addBookToLibrary(e) {
-  e.preventDefault();
-
-  const [title, author, pages] = e.target.elements;
-  myLibrary.push([title.value, author.value, pages.value]);
-
-  while (books.hasChildNodes()) {
-    books.removeChild(books.firstChild);
-  }
-  myLibrary.forEach(displaybooks);
 }
 
 function loadEventListners() {
